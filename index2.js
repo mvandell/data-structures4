d29cd6ba1145be0ca956bd3e78fb71f84087e1c7
Marisa <marisavandellos@gmail.com>
2024-01-03 18:29:32 -0800

    fixed starting code for part 2

--- index2.js
+++ index2.js
@@ -1,2 +1,39 @@
 //PART 2
-const linkedList = [1,2,3,4,5];
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor(head = null) {
+        this.head = head;
+    }
+    add(data) {
+        const newNode = new Node(data);
+        if (this.head === null) {
+            this.head = newNode;
+        }
+        let current = this.head;
+        while (current.next !== null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+}
+
+const linkedList = new LinkedList();
+const node = new Node(1);
+const node2 = new Node(2);
+const node3 = new Node(3);
+const node4 = new Node(4);
+const node5 = new Node(5);
+node.next = node2;
+node2.next = node3;
+node3.next = node4;
+node4.next = node5;
+
+function reverseList(head) {
+    
+}

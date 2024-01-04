45abd93c703b11764a7041390b560a3a515bd094
Marisa <marisavandellos@gmail.com>
2024-01-04 11:06:32 -0800

    finished all parts with chatGPT

--- index4.js
+++ index4.js
@@ -1,2 +1,33 @@
 //PART 4
-const linkedList = [1,2,3,4];
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+const node = new Node(1);
+const node2 = new Node(2);
+const node3 = new Node(3);
+const node4 = new Node(4);
+node.next = node2;
+node2.next = node3;
+node3.next = node4;
+
+function orderChange(head) {
+    let current = head;
+    while (current !== null && current.next !== null) {
+        const temp = current.data;
+        current.data = current.next.data;
+        current.next.data = temp;
+
+        current = current.next.next;
+    }
+    return head;
+}
+
+let current = orderChange(node);
+while (current !== null) {
+    console.log(current.data);
+    current = current.next;
+};
